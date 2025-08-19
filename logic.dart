05cc0a8d03a9159db0dd2292e44d529cc2b645c0
KARTHICK B <karthick1915s@gmail.com>
2025-08-19 12:11:29 +0530

    Create logic.dart

---
+++ logic.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class ColorQuestion {
+  final String word;
+  final Color color;
+  final List<String> options;
+
+  ColorQuestion({
+    required this.word,
+    required this.color,
+    required this.options,
+  });
+}
